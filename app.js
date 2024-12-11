d654a08b907b97045dcac24e3704db4d2f140a72
Dagmar87 <jdfssobrinho@hotmail.com>
2024-12-11 11:52:14 -0300

    Adição da conexão ao banco de dados nos códigos no arquivo app.js

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 var express = require("express");
+var mongoose = require("mongoose");
+
 var app = express();
 
+var url = "mongodb+srv://usuario:senha>@produtosdb.yq3w2.mongodb.net/"
+
+var db = mongoose.connection;
+
 app.get("/", function (req, res) {
-	res.send("Bem-vindo ao Express!");
+	res.send("Conectado ao banco de dados produtosdb do MongoDB.");
 });
+
+mongoose.connect(url);
 
 app.listen(5000, function () {
 	console.log("Servidor escutando na porta 5000");
