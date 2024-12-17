6ddb73d0bc9083723c25a6f04c226a0916d98475
Dagmar87 <jdfssobrinho@hotmail.com>
2024-12-17 09:09:56 -0300

    Adição das rotas raiz e teste nos códigos no arquivo app.js

--- app.js
+++ app.js
@@ -3,16 +3,25 @@ var mongoose = require("mongoose");
 
 var app = express();
 
-var url = "mongodb+srv://usuario:senha>@produtosdb.yq3w2.mongodb.net/"
+var url = "mongodb+srv://jdfssobrinho:justice87@produtosdb.yq3w2.mongodb.net/?retryWrites=true&w=majority&appName=produtosdb"
 
 var db = mongoose.connection;
 
-app.get("/", function (req, res) {
-	res.send("Conectado ao banco de dados produtosdb do MongoDB.");
-});
+db.on('error', console.error);
+db.once('open', function () {
+	console.log("Conectado ao banco de dados produtosdb MongoDB.");
+})
 
 mongoose.connect(url);
 
 app.listen(5000, function () {
 	console.log("Servidor escutando na porta 5000");
 });
+
+app.get('/', function (req, res) {
+	res.send("Bem vindo ao Express!");
+});
+
+app.get('/teste', function (req, res) {
+	res.send("Respondento a solicitação em /teste");
+})
