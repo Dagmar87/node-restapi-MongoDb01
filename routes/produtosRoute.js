2b832bb346438cac87f1b68c6404117abd53b7c4
Dagmar87 <jdfssobrinho@hotmail.com>
2024-12-17 09:21:25 -0300

    Desenvolvimento do código no arquivo produtosRoute.js

---
+++ routes/produtosRoute.js
@@ -0,0 +1,8 @@
+var express = require("express");
+var produtosRouter = express.Router();
+
+produtosRouter.route('').get(function (req, res) {
+	res.send("Exibindo a página principal...");
+});
+
+module.exports = produtosRouter;
