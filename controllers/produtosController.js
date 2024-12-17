a5d5d135398f8a2f7a49aceab5de049133bef620
Dagmar87 <jdfssobrinho@hotmail.com>
2024-12-17 10:08:46 -0300

    Adição do método add nos códigos no arquivo produtosController.js

--- controllers/produtosController.js
+++ controllers/produtosController.js
@@ -1,5 +1,42 @@
+var Produto = require("../models/produtoModel");
+/*
 var get = function (req, res) {
 	res.send("Exibindo a página principal...")
 };
+*/
 
-module.exports = get;
+var get = function (req, res) {
+
+	Produto.find(function (err, produtos) {
+
+		if (err) {
+			res.status(500);
+			res.send("Erro interno do servidor");
+		}
+		else {
+			res.status(200);
+			res.send(produtos);
+		}
+	});
+};
+
+var add = function (req, res) {
+
+	var produto = new Produto(req.body);
+
+	produto.save(function (err) {
+		if (err) {
+			res.status(500);
+			res.send("Erro : falha ao incluir produto...");
+		}
+		else {
+			res.status(201);
+			res.send(produto);
+		}
+	})
+};
+
+module.exports = {
+	add: add,
+	get: get
+};
